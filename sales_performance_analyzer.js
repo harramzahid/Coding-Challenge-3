60026933f6da38e57fd85a8b600dab8a757bbcf6
Harram Zahid <harramzahid5@gmail.com>
2024-09-08 22:09:15 -0400

    create determineperformancerating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -8,4 +8,19 @@ function calculateaveragesales( function sumArray(arr) {
     return averagesales;
 }
 console.log("average sales:"{calculateaveragesales(sales)});
+//Task 2 create a function to determine performance rating
+let averagsales = calculateaveragesales(sales);
+function determineperformancerating(averagesales){
+    if (averagesales > 10000)
+        return "excellent";
+    else if (averagesales >=7000 && <=10000)
+        return " Good "
+    else if (averagesales>=4000 && <= 7000)
+        return " satisfactory "
+    if (averagesales < 4000)
+        return "Needs Improvement"
+}
+console.log("performance rating:", determineperformancerating)
+
+// Task 3 
 
