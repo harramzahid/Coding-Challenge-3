11c5c3f56fc87ad34371b782ec83bcf4b7a37460
harramzahid <harramzahid5@gmail.com>
2024-09-08 21:52:11 -0400

    Implement calculateAverageSales function

---
+++ sales_performance_analyzer.js
@@ -0,0 +1,11 @@
+// Coding Challenge 3
+// Task 1 create a function to get average sales data
+let sales = [ 100, 300, 400] //we are using this as an example
+// function - declaration
+function calculateaveragesales( function sumArray(arr) {
+    return arr.reduce((sum, sales)) => sum + sales;
+    let averagesales= sales/sales.length;
+    return averagesales;
+}
+console.log("average sales:"{calculateaveragesales(sales)});
+
